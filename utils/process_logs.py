4afcce9286d298c9031da3c9329aee67f0426e45
ben stear <bjs385@drexel.edu>
2020-06-03 10:11:29 -0400

    Update process_logs.py

--- utils/process_logs.py
+++ utils/process_logs.py
@@ -17,7 +17,6 @@ print('Total Logs found: '+str(len(all_logs)))
 
 list_of_lists = []
 
-
 for i in range(0,len(all_logs)):
     
     data = json.load(open(all_logs[i]))
@@ -26,4 +25,14 @@ for i in range(0,len(all_logs)):
         
     list_of_lists.append(list(data.values()))
 
-pd.DataFrame(list_of_lists[1:],columns=list_of_lists[0]).to_csv('log_matrix.csv',sep='\t')
+df =pd.DataFrame(list_of_lists[1:],columns=list_of_lists[0])
+
+reordered_cols= ['Package','BADGE','Owner_Repo','Description','date_created','last_commit',
+                     'forks','watchers','stars','contributors','homepage_url','has_wiki','open_issues',
+                     'has_downloads','Run_ID','Date','Pylint_score','Pytest_score','Pip','License','Build','Linux',
+                     'Mac','Windows','Linux_versions','Mac_versions','Windows_versions','Github_event_name']
+
+df = df.reindex(reordered_cols, axis=1)
+
+df.to_csv('log_matrix.csv',sep='\t')
+
