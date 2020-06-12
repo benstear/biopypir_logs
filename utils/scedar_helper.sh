906e8d0836bedd9bb6cc11547a23dc5a9ee6a348
ben stear <bjs385@drexel.edu>
2020-06-12 11:56:14 -0400

    Update scedar_helper.sh

--- utils/scedar_helper.sh
+++ utils/scedar_helper.sh
@@ -221,7 +221,10 @@ elif [ "$1" = "EVALUATE" ]; then
   #cat eval_2.json
   
 elif [ "$1" = "STATISTICS" ]; then
-     
+    
+    OWNER=$(sed -e 's/^"//' -e 's/"$//' <<<"$OWNER")
+    echo $OWNER
+    
     curl https://api.github.com/repos/"$OWNER"/"$PACKAGE" | jq "{Owner_Repo: .full_name, 
       Package: .name, Description: .description, date_created: .created_at, last_commit: .pushed_at, forks: .forks, watchers: 
       .subscribers_count, stars: .stargazers_count, contributors: .contributors_url,
