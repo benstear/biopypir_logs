1a6af27a781ca4d83184fb7095b423f374cd801f
ben stear <bjs385@drexel.edu>
2020-06-28 11:36:58 -0400

    Update process_logs.py

--- utils/process_logs.py
+++ utils/process_logs.py
@@ -33,7 +33,7 @@ df =pd.DataFrame(list_of_lists[1:],columns=list_of_lists[0])
 reordered_cols= ['Package','BADGE','Owner_Repo','Description','date_created','last_commit',
                      'forks','watchers','stars','contributors','homepage_url','has_wiki','open_issues',
                      'has_downloads','Run_ID','Date','Pylint_score','Pytest_score','Pip','License','Build','Linux',
-                     'Mac','Windows','Linux_versions','Mac_versions','Windows_versions','badge_color','Github_event_name']
+                     'Mac','Windows','Linux_versions','Mac_versions','Windows_versions','Github_event_name']
 
 df = df.reindex(reordered_cols, axis=1)
 
