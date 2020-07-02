5c6825411767616eaca9f99cfd007b2ff386389a
ben stear <bjs385@drexel.edu>
2020-07-02 10:30:50 -0400

    Update workflow_helper.sh

--- utils/workflow_helper.sh
+++ utils/workflow_helper.sh
@@ -157,4 +157,29 @@ elif [ "$1" = "EVALUATE" ]; then
     if [ ! -z "{$mac_arr[*]}" ]; then mac_arr_=$(echo "${mac_arr[*]}");  else mac_arr_=$('NA'); fi
     if [ ! -z "{$windows_arr[*]}" ]; then windows_arr_=$(echo "${windows_arr[*]}");  else windows_arr_=$('NA'); fi
     IFS=$' \t\n';
+    
+   jq -n --arg Workflow_Run_Date "$date_clip" \
+      --arg linux_vers "${linux_unq[*]}" \
+     --arg mac "${mac_arr_[*]}" \
+     --arg mac_vers "${mac_unq[*]}" \
+     --arg windows "${windows_arr_[*]}" \
+     --arg windows_vers "${windows_unq[*]}" \
+     --arg coverage_score "$pytest_score_final" \
+      --arg linux "${linux_arr_[*]}" \
+      --arg lint_score "$pylint_score_final"\
+       '{ Workflow_Run_Date :  $Workflow_Run_Date,
+          Pylint_score  :  $lint_score,  
+          Pytest_score  :  $coverage_score,
+          Pip           : "True",
+          License       : "True",
+          Build         : "True",
+          Linux         : $linux,
+          Mac           : $mac,
+          Windows       : $windows,
+          Linux_versions: $linux_vers,
+          Mac_versions: $mac_vers,
+          Windows_versions: $windows_vers }'  > scores_and_matrix.json
+
+
+    
 fi 
