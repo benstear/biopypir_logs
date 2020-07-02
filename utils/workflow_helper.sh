38bd1dbcad19b1167895e29229fb6256c9d4a4ed
ben stear <bjs385@drexel.edu>
2020-07-02 15:21:09 -0400

    Update workflow_helper.sh

--- utils/workflow_helper.sh
+++ utils/workflow_helper.sh
@@ -217,9 +217,12 @@ elif [ "$1" = "STATISTICS" ]; then
       # get names of contributors
       curl https://api.github.com/repos/"$OWNER"/"$PACKAGE"/contributors | jq ".[].login"  > contrib_logins.txt
       tr -d '"' <contrib_logins.txt > contributors.txt # delete quotes from file
-      cat contributors.txt
+      
 
-      #tr '\n' ' ' < contributors.txt > contributors2.txt  # replace \n with ' '
+      (tr '\n' ' ' < contributors.txt) > contributors2.txt  # replace \n with ' '
+      
+      echo 'contributors2.txt =  '
+      cat contributors2.txt
       
       sed -e  's#^#https://github.com/#' contributors.txt > contributors_gh.txt    # add github url to login names
       
@@ -232,8 +235,8 @@ elif [ "$1" = "STATISTICS" ]; then
       
       echo 'PIP: '
       echo $PIP
-      if [ $PIP == 'True' ]; then pip_url=https://pypi.org/project/"$PACKAGE"/;
-      else pip_url='NA';
+      if [ "$PIP" ]; then pip_url=https://pypi.org/project/"$PACKAGE"/;
+      else pip_url == 'NA';
       fi
       pip_url=https://pypi.org/project/"$PACKAGE"/
       echo 'pip_url:'
@@ -242,7 +245,7 @@ elif [ "$1" = "STATISTICS" ]; then
       jq -n --arg github_event "$GITHUB_EVENT_NAME" --arg run_id "$GITHUB_RUN_ID" \
       --arg contributors_url "$contributors_url" \
       --arg num_contributors "$n_cntrbtrs" \
-      --arg contributor_names "$(cat contributors.txt)" \
+      --arg contributor_names "$(cat contributors2.txt)" \
       --arg pip_url "$pip_url" \
       '{ Github_event_name: $github_event, Run_ID: $run_id, PIP_url: $pip_url ,contributor_names: $contributor_names, contributors_url: $contributors_url, num_contributors: $num_contributors}' > run_info.json
 
