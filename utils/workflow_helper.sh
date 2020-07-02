198d08335b94739c427b05910babe5b3691e80e0
ben stear <bjs385@drexel.edu>
2020-07-02 11:43:11 -0400

    Update workflow_helper.sh

--- utils/workflow_helper.sh
+++ utils/workflow_helper.sh
@@ -192,13 +192,13 @@ elif [ "$1" = "EVALUATE" ]; then
    
    LINT_SCORE=$(sed -e 's/^"//' -e 's/"$//' <<<"$LINT_SCORE") # Remove quotes
   
-  if [ "$LICENSE" ] && [ "$BUILD" ] && [ "$PIP" ]; then badge='BRONZE';
-    if [ $COVERAGE_SCORE != 'NA' ]; then
+  #if [ "$LICENSE" ] && [ "$BUILD" ] && [ "$PIP" ]; then badge='BRONZE';
+  #  if [ $COVERAGE_SCORE != 'NA' ]; then
         #if  (( $(echo "$LINT_SCORE > 6.0" |bc -l) ))  && [ $COVERAGE_SCORE -gt 40 ]; then badge='GOLD';  
         #elif (( $(echo "$LINT_SCORE > 3.0" |bc -l) )) && [ $COVERAGE_SCORE -gt 20 ]; then badge='SILVER'; 
         #fi 
-    fi 
-  fi
+    #fi 
+  #fi
   
   jq -n --arg badge "$badge" '{BADGE : $badge}' > badge.json; 
   jq -s add eval.json badge.json  > eval_2.json
