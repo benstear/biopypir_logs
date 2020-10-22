1b2df288e0346b8d6fbe052ba071b49e3758d061
ben stear <bjs385@drexel.edu>
2020-10-22 10:10:25 -0400

    Update workflow_helper.sh

--- utils/workflow_helper.sh
+++ utils/workflow_helper.sh
@@ -205,27 +205,29 @@ elif [ "$1" = "EVALUATE" ]; then
                                                  Pip_url       : $pip_url }'  > scores_and_matrix.json
 
     cat scores_and_matrix.json | jq 'del(.OS, .Python_version)' > eval.json
-  
+    
+elif [ "$1" = "BADGING" ]; then
+
    # ================= GET BADGE STATUS ======================== #
    badge='NONE'
    badge='BRONZE'
 
 if [[ $pytest_score_final != "NA" ]]; then pytest_score_final=$(sed -e 's/^"//' -e 's/"$//' <<<$pytest_score_final); fi  # Remove quotes  
 
-  #if [ "$license_result" ] && [ "$build_result" ] && [ "$pip_result" ]; then badge='BRONZE'; badge_color='#cd7f32;' 
-  #  if [ $pytest_score_final != 'NA' ]; then
-  #      if  (( $(echo "$pylint_score_final > 6.0" |bc -l) ))  && [ $pytest_score_final -gt 40 ]; then badge='GOLD';  badge_color='#FFD700'
-  #      elif (( $(echo "$pylint_score_final > 3.0" |bc -l) )) && [ $pytest_score_final -gt 20 ]; then badge='SILVER'; badge_color='#C0C0C0';
-  #      fi 
-  #  fi 
-  #fi
+  if [ "$license_result" ] && [ "$build_result" ] && [ "$pip_result" ]; then badge='BRONZE'; badge_color='#cd7f32'; 
+    if [ $pytest_score_final != 'NA' ]; then
+        if  (( $(echo "$pylint_score_final > 6.0" |bc -l) ))  && [ $pytest_score_final -gt 40 ]; then badge='GOLD';  badge_color='#FFD700';
+        elif (( $(echo "$pylint_score_final > 3.0" |bc -l) )) && [ $pytest_score_final -gt 20 ]; then badge='SILVER'; badge_color='#C0C0C0';
+        fi 
+    fi 
+  fi
   
   jq -n --arg badge "$badge" '{BADGE : $badge}' > badge.json;  # dont need this ?
   jq -s add eval.json badge.json  > eval_2.json                # dont need this ?
   
   echo "BADGE=$badge" >> $GITHUB_ENV  
   
-  badge_color='#cd7f32' #bronze
+  #badge_color='#cd7f32' #bronze
   #badge_color='#C0C0C0'  # silver
   #badge_color='#FFD700'  # gold  
   
@@ -235,7 +237,7 @@ if [[ $pytest_score_final != "NA" ]]; then pytest_score_final=$(sed -e 's/^"//' 
         --arg badge_color "$badge_color"  \
         --arg logo  "Auth0" \
         --arg logoColor "white" \
-        --arg width 45 \
+        --arg width 40 \
         --arg style "for-the-badge" \
                          '{ schemaVersion: 1,
                               label: $biopypir_name,
