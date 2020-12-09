4ff0a78ef93b198cebb29296699481f9bd262b8b
ben stear <bjs385@drexel.edu>
2020-12-09 14:44:09 -0500

    Update get_issues.py

--- utils/get_issues.py
+++ utils/get_issues.py
@@ -19,36 +19,55 @@ import os
 #print('Argument List:', str(sys.argv[1]))
 #issues_url = os.path.join('https://api.github.com/repos/', sys.argv[1], '/issues')
 
-issues_url = 'https://api.github.com/repos/' + sys.argv[1] + '/issues'
 
+name_repo =  sys.argv[1]
 
-res = requests.get(issues_url)#,headers=headers)
-issues_obj = res.json()
-
-total_num_issues = len(issues_obj)
-
-open_issues = []
-closed_issues = []
-response_timeLs = []
-
-for i in issues_obj:
-    #print(i.keys())
-    created_at = datetime.datetime.strptime(i['created_at'], "%Y-%m-%dT%H:%M:%SZ")
-    updated_at = datetime.datetime.strptime(i['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
-    response_time =  updated_at - created_at
+def find_issues(name_repo):
     
-    response_timeLs.append(response_time.days)
+    try:    
+        repo_res = requests.get('https://api.github.com/repos/' + name_repo)
+        repo_obj = repo_res.json()
+        num_open_issues = repo_obj['open_issues_count']
+        
+    except requests.exceptions.RequestException as e:  
+        raise SystemExit(e)
     
-    #print(response_time.days)
+    
+    if repo_obj['has_issues']:
+        
+        try:
+            res = requests.get('https://api.github.com/repos/' + name_repo + '/issues')
+            issues_obj = res.json()
+         
+            response_timeLs = [] #open_issues = closed_issues = []
 
-    if i['state'] == 'open':
-        open_issues.append((i['number'],i['title']))
-    elif i['state'] == 'closed':
-        closed_issues.append((i['number'],i['title']))    
+            for i in issues_obj:
+                
+                created_at = datetime.datetime.strptime(i['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+                updated_at = datetime.datetime.strptime(i['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+                
+                response_time =  updated_at - created_at
+                response_timeLs.append(response_time.days)
+                
+               # if i['state'] == 'open':  open_issues.append((i['number'],i['title']))
+               # elif i['state'] == 'closed':   closed_issues.append((i['number'],i['title']))    
+               # else:    print(i['state'])
+                                        
+            os.environ["NUM_ISSUES"] = str(len(issues_obj))
+            os.environ["NUM_OPEN_ISSUES"] = str(num_open_issues)
+            os.environ["AVE_RES"] = str(sum(response_timeLs)/len(response_timeLs))
+            #print(str(sum(response_timeLs)/len(response_timeLs)))
+            
+        except requests.exceptions.RequestException as e:  
+            raise SystemExit(e)
+            
     else:
-        print(i['state'])
-        
-print(sum(response_timeLs)/len(response_timeLs))
-        
-os.environ["NUM_ISSUES"] = str(len(issues_obj))
-        
+            os.environ["NUM_ISSUES"] = '0'
+            os.environ["NUM_OPEN_ISSUES"] = str(num_open_issues)
+            os.environ["AVE_RES"] = 'NA'
+            
+            
+    
+if __name__ == "__main__":
+    find_issues(name_repo)
+            
