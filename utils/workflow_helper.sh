9584518aa8dbc8c192f5eb65e3ee4dbc54eb4ebc
ben stear <bjs385@drexel.edu>
2020-12-21 10:47:33 -0500

    Update workflow_helper.sh

--- utils/workflow_helper.sh
+++ utils/workflow_helper.sh
@@ -311,7 +311,7 @@ elif [ "$1" = "STATISTICS" ]; then
 
      
       # get names of contributors
-      #curl https://api.github.com/repos/"$OWNER"/"$PACKAGE"/contributors | jq ".[].login"  > contrib_logins.txt      
+      curl https://api.github.com/repos/"$OWNER"/"$PACKAGE"/contributors | jq ".[].login"  > contrib_logins.txt      
 
       echo '------------    calling  py script....    ----------------'
 
