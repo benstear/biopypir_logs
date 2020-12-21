594f244f2c36c7b3a2734c342e76076d20b73c63
ben stear <bjs385@drexel.edu>
2020-12-21 16:46:59 -0500

    Update py_helper.py

--- utils/py_helper.py
+++ utils/py_helper.py
@@ -88,7 +88,10 @@ if __name__ == "__main__":
         
     elif sys.argv[1] == 'CONTRIBUTORS':
         #get_contributors(name_repo)
-        print('"{}" "{}" "{}"'.format(*get_contributors(name_repo)))
+        #print('"{}" "{}" "{}"'.format(*get_contributors(name_repo)))
+        
+        res = get_contributors(name_repo)
+        print(('"{}" '*len(res)).format(*res))
         
     elif  sys.argv[1] == 'PROCESS LOGS':
         pass
