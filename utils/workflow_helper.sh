244d0b72f1403ed2488ca30ee70f5202a5f2f236
ben stear <bjs385@drexel.edu>
2020-12-22 10:09:53 -0500

    Update workflow_helper.sh

--- utils/workflow_helper.sh
+++ utils/workflow_helper.sh
@@ -326,7 +326,7 @@ elif [ "$1" = "STATISTICS" ]; then
       #python3 utils/py_helper.py "CONTRIBUTORS" "$OWNER/$PACKAGE" > cont.json
       #cat cont.json
       
-      cp  stats.json  stats.json.tmp && jq  -s add stats.json.tmp $contributors > stats.json && rm stats.json.tmp #badge.json
+      cp  stats.json  stats.json.tmp && jq  -s add stats.json.tmp (echo $contributors) > stats.json && rm stats.json.tmp #badge.json
       
       
       echo '--------------'
