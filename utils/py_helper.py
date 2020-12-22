159d45f0bba4e721d30773aeed7d22568ecdc37d
ben stear <bjs385@drexel.edu>
2020-12-22 18:41:49 -0500

    Update py_helper.py

--- utils/py_helper.py
+++ utils/py_helper.py
@@ -70,30 +70,64 @@ def get_contributors(name_repo):
      print(json.dumps(return_dict)) 
             
 
-       
+            
+            
+            
+            
+            
+            
+            
+def process_logs():
+
+    logs_path = "logs/*.json"
+    all_logs = glob.glob(logs_path)  # load all logs from logs/ directory. Only the most recent 
+                                     # workflow run results for each package will be there. 
+    print('Total Logs found: '+str(len(all_logs)))
+
+    list_of_lists = []
+    for i in range(0,len(all_logs)):
+        try:
+            data = json.load(open(all_logs[i]))
+        except json.decoder.JSONDecodeError as e:  
+            raise SystemExit(e)
+
+        if i==0: list_of_lists.append(list(data.keys())) # if it's the first pass of the loop, save the keys to use as
+                                                         #  headers for the log_matrix.csv and biopypir_matrix.md files
+        list_of_lists.append(list(data.values()))        # otherwise append each packages results to list_of_lists
+
+    # if you add more, or get rid of columns (fields in .json file), you will get an error, 
+    # when the script tries to read the old logs in because they will have the old number of columns.
+    # Delete all old logs in logs/ and rerun new workflow.
+    df =pd.DataFrame(list_of_lists[1:],columns=list_of_lists[0]) 
+
+    reordered_cols= ['Package','BADGE','Owner_Repo','Description','Workflow_Run_Date','date_created','last_commit',
+                         'forks','watchers','stars','homepage_url','has_wiki','open_issues',
+                         'has_downloads','Run_ID','Pylint_score','Pytest_score','Pip','Pip_url','License','Build','Linux',
+                         'Mac','Windows','Linux_versions','Mac_versions','Windows_versions','contributor_names','contributor_url',
+                     'num_contributors','Github_event_name', 'Num_Issues', 'Num_Open_Issues', 'Average_Response_Time' ]
+
+    df = df.reindex(reordered_cols, axis=1)
+    df.to_csv('log_matrix.csv',index=False,sep='\t')
+
+    # Create and save markdown table of packages
+    md_table =  df.to_markdown()
+    with open('biopypir_matrix.md', 'w') as f:
+        f.write(md_table)
+
+            
+
             
                
 if __name__ == "__main__":
-    
-    name_repo =  sys.argv[2]
-    
     if sys.argv[1] == 'ISSUES':
-        get_issues(name_repo)
+        get_issues(sys.argv[2])
         
     elif sys.argv[1] == 'CONTRIBUTORS':
-        get_contributors(name_repo)
-        #print('"{}" "{}" "{}"'.format(*get_contributors(name_repo)))
-        
-        #res = get_contributors(name_repo)
-        #print(('"{}" '*len(res)).format(*res))
-        
+        get_contributors(sys.argv[2])
+
     elif  sys.argv[1] == 'PROCESS LOGS':
-        pass
+        process_logs()
         
-
-    #print(f"::set-output name=myOUTPUT::{my_output}")
-    #bashCommand = "MAGICVAR="42""
-    #process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-
+# Try to set env vars from this script
 
 
